e41640df09fac9f71673721b4f33af760a73fa8a
santoshp61 <santoshpakhrin61@gmail.com>
2026-01-27 23:53:55 +0545

    minor chnage

--- src/Components/Context/Auth.js
+++ src/Components/Context/Auth.js
@@ -11,8 +11,6 @@ export const AuthProvider = ({ children }) => {
     const logout = () => {
         setUser(null);
     };
-
-
     return (
         <AuthContext.Provider value={{ user, login, logout }}>
             {children}
